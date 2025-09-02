9a915fcc21db72f36e7a46ed935179a7c34b632d
John Milller <staremperor@gmail.com>
2025-09-01 19:39:11 -0700

    Configure Amplify first

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Ensure this is correct
-import './index.css'; // Ensure this file exists
+import './index.css';
 import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from "aws-amplify";
-import amplifyConfig from "../amplify_outputs.json";
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
-Amplify.configure(amplifyConfig);
+// Configure Amplify **before** importing any code that uses it
+Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Dynamically import App after Amplify is configured to avoid config warnings
+import('./App').then(({ default: App }) => {
+  root.render(
+    <React.StrictMode>
       <Authenticator signUpAttributes={['preferred_username', 'email']}>
-           {({ signOut, user }) => (
-         <App user={user} signOut={signOut}/>
-         )}
+        {({ signOut, user }) => (
+          <App user={user} signOut={signOut} />
+        )}
       </Authenticator>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+});
