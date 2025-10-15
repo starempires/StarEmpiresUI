f99d62aee335a2a675e3336897ca93188429ab4f
John Milller <staremperor@gmail.com>
2025-10-15 16:17:51 -0700

    Update vite.config.js to load locahost.pem files only in dev environment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import fs from "fs";
 import react from '@vitejs/plugin-react';
 
+const useHttps = process.env.NODE_ENV === 'development';
+
 export default defineConfig({
   plugins: [react()],
   esbuild: {
@@ -9,10 +11,10 @@ export default defineConfig({
   },
   server: {
     port: 3000, // Change if needed
-    https: {
+    https: useHttps ? {
           key: fs.readFileSync("./localhost-key.pem"),
           cert: fs.readFileSync("./localhost.pem"),
-    },
+    } : false,
   },
   resolve: {
     alias: {
