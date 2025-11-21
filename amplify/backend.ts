169a4279e5e398d60d814e90dbf67e13ef60f555
John Milller <staremperor@gmail.com>
2025-11-20 22:27:07 -0800

    Use nodejs22.x in lambdas

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,7 +5,19 @@ import { data } from './data/resource';
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
+const backend = defineBackend({
   auth,
   data,
 });
+
+// Configure Node.js 22.x runtime for all Lambda functions created by Amplify
+// This updates the AppSync resolver Lambda functions to use Node.js 22.x
+const dataStack = backend.data.resources.cfnResources;
+if (dataStack.cfnGraphqlApi) {
+  // Iterate through all child resources to find Lambda functions
+  dataStack.cfnGraphqlApi.node.findAll().forEach((child) => {
+    if (child.node.defaultChild?.cfnResourceType === 'AWS::Lambda::Function') {
+      child.node.defaultChild.addPropertyOverride('Runtime', 'nodejs22.x');
+    }
+  });
+}
